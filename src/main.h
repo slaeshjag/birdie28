14f2b24f7fc700dbcafcf6aadfb3ee72257fe693
h4xxel <isaksson.axel@telia.com>
2018-05-10 14:50:32 +0200

    menu builds

---
+++ src/main.h
@@ -0,0 +1,103 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <darnit/darnit.h>
+//#include "sfx.h"
+
+//#include "movable.h"
+//#include "player.h"
+//#include "bullet.h"
+//#include "blocklogic.h"
+//#include "turret.h"
+#define DISPLAY_WIDTH 800
+#define DISPLAY_HEIGHT 600
+#define PORT 1339
+
+#define GAME_TITLE "birdie28"
+
+#define NAME_LEN_MAX 64
+#define	PLAYER_CAP 32
+
+#define PANE_R 0xCD
+#define PANE_G 0xCD
+#define PANE_B 0xCD
+
+
+typedef enum GameState GameState;
+enum GameState {
+	GAME_STATE_MENU,
+	GAME_STATE_SELECT_NAME,
+	GAME_STATE_CHARACTERS,
+	GAME_STATE_LOBBY,
+	GAME_STATE_ENTER_IP,
+	GAME_STATE_HOST,
+	GAME_STATE_GAMEROOM,
+	GAME_STATE_GAME,
+	GAME_STATE_GAME_OVER,
+	GAME_STATE_QUIT,
+	GAME_STATES,
+};
+
+typedef struct Gfx Gfx;
+struct Gfx {
+	struct {
+		DARNIT_FONT *small;
+		DARNIT_FONT *large;
+	} font;
+};
+
+extern Gfx gfx;
+extern char player_name[];
+extern int server_sock;
+extern char *team_name[];
+
+
+struct PlayerTimer {
+	uint32_t		team1;
+	uint32_t		team2;
+	int			advantage;
+};
+
+
+struct GameStateStruct {
+	int	player_id;
+	bool is_host;
+	//MOVABLE			movable;
+	//DARNIT_MAP		*active_level;
+//	DARNIT_TILESHEET	*_7seg;
+//	BULLET			bullet;
+//	struct HealthbarStruct	healthbar;
+//	struct PlayerState	player[PLAYER_CAP];
+//	struct PlayerTimer	timer;
+//	struct BlockLogicBlock	block[2]; // One per team for now
+//	struct BulletList	bullet;
+//	struct Turret		turret;
+
+//	struct TMRenderHackContext *tmrender[16];
+	
+//	DARNIT_PARTICLE *explosion[3];
+	
+//	uint8_t tower[16][BLOCKLOGIC_AREA_HEIGHT*BLOCKLOGIC_AREA_WIDTH];
+//	int towers[2];
+//	struct {
+//		int x;
+//		int y;
+//	} center_of_gravity[16];
+	
+//	SfxStruct sfx;
+	struct {
+				int x;
+				int y;
+				int follow;
+	} camera;
+};
+
+extern struct GameStateStruct *s;
+
+void game_state(GameState state);
+void restart_to_menu(const char *name);
+int join_game(unsigned long sip);
+
+#endif
---
+++ src/network/protocol.h
@@ -0,0 +1,179 @@
+#ifndef __COMMON_PROTOCOL_H__
+#define __COMMON_PROTOCOL_H__
+
+#include <stdint.h>
+#include "../main.h"
+
+
+typedef enum PacketType PacketType;
+enum PacketType {
+	PACKET_TYPE_LOBBY,
+	PACKET_TYPE_JOIN,
+	PACKET_TYPE_TEAM,
+	PACKET_TYPE_START,
+	PACKET_TYPE_MOVE_OBJECT,
+	PACKET_TYPE_SOUND,
+	PACKET_TYPE_KEYPRESS,
+	PACKET_TYPE_BLOCK_PLACE,
+	PACKET_TYPE_BLOOD,
+	PACKET_TYPE_EXPLOSION,
+	PACKET_TYPE_TIMER,
+	PACKET_TYPE_BULLET_ANNOUNCE,
+	PACKET_TYPE_BULLET_UPDATE,
+	PACKET_TYPE_BULLET_REMOVE,
+	PACKET_TYPE_EXIT,
+};
+
+typedef struct PacketLobby PacketLobby;
+struct PacketLobby {
+	uint16_t type;
+	uint16_t size;
+	
+	char name[NAME_LEN_MAX];
+};
+
+typedef struct PacketJoin PacketJoin;
+struct PacketJoin {
+	uint16_t type;
+	uint16_t size;
+	
+	uint32_t id;
+	char name[NAME_LEN_MAX];
+	int team;
+};
+
+typedef struct PacketStart PacketStart;
+struct PacketStart {
+	uint16_t type;
+	uint16_t size;
+	
+	uint32_t player_id;
+};
+
+typedef struct PacketKeypress PacketKeypress;
+struct PacketKeypress {
+	uint16_t type;
+	uint16_t size;
+
+	//InGameKeyStateEntry keypress, keyrelease;
+};
+
+typedef struct PacketBlood PacketBlood;
+struct PacketBlood {
+	uint16_t type;
+	uint16_t size;
+	
+	uint32_t player;
+	uint32_t x;
+	uint32_t y;
+};
+
+typedef struct PacketExplosion PacketExplosion;
+struct PacketExplosion {
+	uint16_t type;
+	uint16_t size;
+	
+	uint32_t team;
+	uint32_t x;
+	uint32_t y;
+};
+
+typedef struct PacketSound PacketSound;
+struct PacketSound {
+	uint16_t type;
+	uint16_t size;
+
+	uint8_t sound;
+};
+
+
+typedef struct PacketBlockPlace PacketBlockPlace;
+struct PacketBlockPlace {
+	uint16_t type;
+	uint16_t size;
+	
+	uint8_t team;
+	uint8_t	x;
+	uint8_t	y;
+	uint8_t block;
+};
+
+
+typedef struct PacketBulletAnnounce PacketBulletAnnounce;
+struct PacketBulletAnnounce {
+	uint16_t type;
+	uint16_t size;
+
+	uint8_t bullet_type;
+	uint16_t x;
+	uint16_t y;
+	uint8_t id;
+};
+
+
+typedef struct PacketBulletRemove PacketBulletRemove;
+struct PacketBulletRemove {
+	uint16_t type;
+	uint16_t size;
+
+	uint8_t id;
+};
+
+
+typedef struct PacketBulletUpdate PacketBulletUpdate;
+struct PacketBulletUpdate {
+	uint16_t type;
+	uint16_t size;
+
+	uint16_t x;
+	uint16_t y;
+	uint8_t id;
+};
+
+
+typedef struct PacketTimer PacketTimer;
+struct PacketTimer {
+	uint16_t type;
+	uint16_t size;
+
+	uint8_t advantage;
+	uint32_t team1;
+	uint32_t team2;
+};
+
+
+typedef struct PacketExit PacketExit;
+struct PacketExit {
+	uint16_t type;
+	uint16_t size;
+	
+	uint32_t team;
+};
+
+typedef union Packet Packet;
+union Packet {
+	struct {
+		uint16_t type;
+		uint16_t size;
+		uint8_t raw[1024];
+	};
+	
+	PacketLobby lobby;
+	PacketJoin join;
+	PacketStart start;
+	PacketSound sound;
+	PacketKeypress keypress;
+	PacketExit exit;
+	PacketBlockPlace block_place;
+	PacketExplosion explosion;
+	PacketBlood blood;
+	PacketTimer timer;
+	PacketBulletUpdate bullet_update;
+	PacketBulletAnnounce bullet_announce;
+	PacketBulletRemove bullet_remove;
+};
+
+int protocol_send_packet(int sock, Packet *pack);
+int protocol_recv_packet(int sock, Packet *pack);
+
+#endif
